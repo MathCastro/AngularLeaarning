72628110fc06f4b247faa1301f20deed8aa65834
mcastro <mcastro@iMac-de-BRISA.local>
2018-12-13 18:39:40 -0200

    Go back button and other changes

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -5,6 +5,8 @@ import { USUARIOS } from '../mock-usuarios';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 import { SusersService } from '../susers.service'
+import {SelectionModel} from '@angular/cdk/collections';
+import {MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-welcome',
@@ -15,6 +17,7 @@ export class WelcomeComponent implements OnInit {
 
   usuarios: Usuario[];
   displayedColumns: string[] = ['id', 'login', 'senha'];
+  selection = new SelectionModel<Usuario>(true, []);
 
   constructor(
     private http: HttpClient,
@@ -40,5 +43,19 @@ export class WelcomeComponent implements OnInit {
     var localizacao = '/detail/' + row
     this.router.navigate([localizacao]);
   }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+    isAllSelected() {
+      const numSelected = this.selection.selected.length;
+      const numRows = this.usuarios.length;
+      return numSelected === numRows;
+    }
+  
+    /** Selects all rows if they are not all selected; otherwise clear selection. */
+    masterToggle() {
+      this.isAllSelected() ?
+          this.selection.clear() :
+          this.usuarios.forEach(row => this.selection.select(row));
+    }
 
 }
